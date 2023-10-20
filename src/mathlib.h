8aa248bd9e171fa1acf8f4e37d1785b954961ea4
Anton Aparin <anton@antonaparin.com>
2023-10-20 22:30:48 +0200

    Optimize sub-optimal code

--- src/mathlib.h
+++ src/mathlib.h
@@ -1,134 +1,103 @@
-float sum(float a, float b) 
+float sum(float a, float b)
 {
-	float return0; return0 = a + b;
-	return return0;
+    return a + b;
 }
 
-float min(float a, float b) 
+float min(float a, float b)
 {
-	float return0; return0 = a - b;
-	return return0;
+    return a - b;
 }
 
-float mult(float a, float b) 
+float mult(float a, float b)
 {
-	float return0; return0 = a * b;
-	return return0;
+    return a * b;
 }
 
-float divv(float a, float b) 
+float divv(float a, float b)
 {
-	float return0; return0 = a / b;
-	return return0;
+    return a / b;
 }
 
-long int poww(long int a, unsigned int n) 
+long int poww(long int a, unsigned int n)
 {
-	long int res = 1;
-	for(int i = 0; i<n; i++)
-	{
-		res *= a;
-	}
-	return res;
+    long int res = 1;
+    for (int i = 0; i < n; i++)
+    {
+        res *= a;
+    }
+    return res;
 }
 
-float abss(float a) 
+float abss(float a)
 {
-	if(a>0)
-	{
-		return a;
-	}
-	if(a<0)
-	{
-		a = a * -1;
-		return a;
-	}
+    if (a < 0)
+    {
+        a = -a;
+    }
+    return a;
 }
 
 float truncc(float y)
 {
-	int x = y;
-	return x;
+    int x = y;
+    return x;
 }
 
-
-float ffmax(float a, float b) 
+float ffmax(float a, float b)
 {
-	if(a > b)
-	{
-		return a;
-	}
-	else
-	{
-		return b;
-	}
+    return (a > b) ? a : b;
 }
 
-float ffmin(float a, float b) // наименьшее значение среди x и y 
+float ffmin(float a, float b)
 {
-	if(a < b)
-	{
-		return a;
-	}
-	else
-	{
-		return b;
-	}
+    return (a < b) ? a : b;
 }
 
-
-float dim(float x, float y) 
+float dim(float x, float y)
 {
-	float res; res = x - y;
-	if(res < 0)
-		{return 0;}
-	else
-		{return res;}
+    float res = x - y;
+    if (res < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return res;
+    }
 }
 
 float _exp2(float x)
 {
-	float res = 1;
-	for(int i = 0; i<x; i++)
-	{
-		res*=2;
-	}
-	return res;
+    float res = 1;
+    for (int i = 0; i < x; i++)
+    {
+        res *= 2;
+    }
+    return res;
 }
 
-float cpsg(float x, float y) 
+float cpsg(float x, float y)
 {
-	float res = 0;
-	if(y<0)
-	{
-		res = x*-1;
-	}
-	else
-	{
-		res = x;
-	}
-	return res;
+    return (y < 0) ? -x : x;
 }
 
-double _exp(float x) 
+double _exp(float x)
 {
-	double e = 2.71828182845904;
-	double res = 1;
-	for(int i = 0; i<x; i++)
-	{
-		res *= e;
-	}
-	return res;
+    double e = 2.71828182845904;
+    double res = 1;
+    for (int i = 0; i < x; i++)
+    {
+        res *= e;
+    }
+    return res;
 }
 
-int bisector(int a) 
+int bisector(int a)
 {
-	int res; res = a / 2;
-	return res;
+    return a / 2;
 }
 
-int bisector_(int a) // нахождение угла от биссектрисы
+int bisector_(int a)
 {
-	int res; res = a * 2;
-	return res;
+    return a * 2;
 }
